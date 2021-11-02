38c7842f3d9124d2128bd4b3db5be8267e7e5279
Jagadeeshwari Uppada <91789022+jagadeeshwariu@users.noreply.github.com>
2021-11-02 20:27:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,15 @@
 print("Hello")
 print("Python")
 print("AATGGCCTGTTGG")
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      0
+dna_seq="AATTGGCGTTATGCAATTGCCC" 
+print(dna_seq)
+print(len(dna_seq))
+length=len(dna_seq)
+print(dna_seq[:int(length/2)])
+print(dna_seq[int(length/2):])
+lower_dna_seq=dna_seq.lower()
+print(lower_dna_seq)
+print(dna_seq.count("A"))
+print(dna_seq.find("T"))
+gc_percentage= 100*(dna_seq.count("G")+dna_seq.count("C"))/len(dna_seq)
+print(gc_percentage)
